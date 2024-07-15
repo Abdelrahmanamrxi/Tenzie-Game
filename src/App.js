2635d720ccc21de3051575a08ab3ec5251be9d93
Abdelrahman <abdelrahmanamrxi@gmail.com>
2024-07-15 18:35:05 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import Die from './Die'
+import React from 'react'
 
 function App() {
+ 
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+}
+const [dice,setdice]=React.useState(allNewDice)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <main className='maine'>
+    
+<div className='container'>
+
+{dice.map(items=>{
+  return <Die value={items}/>
+})}
+ </div>
+ 
+   </main>
+  
+  )
 }
 
-export default App;
+export default App
---
+++ src/Die.js
@@ -0,0 +1,15 @@
+import './App.css' 
+
+export default function Die(prop) {
+  return (
+    
+     
+        <div className='value'>
+   <h2 className='number'>{prop.value}</h2>
+   </div>
+
+  
+      
+   
+  )
+}
